83ece06892ef794926b9a939a65d0847337be052
Phiphat050647 <112453645+Phiphat050647@users.noreply.github.com>
2023-05-08 17:25:04 +0700

    first commit

--- Project_Cs311_JubJib.py
+++ Project_Cs311_JubJib.py
@@ -1,62 +1,272 @@
+import sqlite3
 from tkinter import *
 from tkinter import messagebox
-from tkinter import ttk
-import tkinter.font as tkFont
+
+def creatconnection():
+    global conn,cursor
+    conn = sqlite3.connect('JukJik_database.db')
+    cursor = conn.cursor()
 
 def mainwindow():
     root = Tk()
-    w = 1080
-    h = 800
+    w = 375
+    h = 812
     x = root.winfo_screenwidth()/2 - w/2
     y = root.winfo_screenheight()/2 - h/2
     root.geometry("%dx%d+%d+%d"%(w,h,x,y))
-    root.config(bg='#ADD8E6')
+    root.config(bg='#1D708B')
     root.title("Login Application: ")
     root.option_add('*font',"Helvetica 18 bold")
-    root.rowconfigure((0,1,2,3),weight=1)
-    root.columnconfigure((0,1,2,3),weight=1)
+    root.rowconfigure((0),weight=1)
+    root.columnconfigure((0),weight=1)
+    canvas = Canvas(root,width=375,height=812)
+    canvas.place(x=0,y=0)
     
     return root
 
-def loginlayout() :
-    global fullfram,userinfo,pwdinfo
-    userinfo = StringVar()
-    pwdinfo = StringVar()       
+def firt_pace() :
+    global splash_page
+    splash_page = Frame(root,bg='#ffffff')
+    splash_page.grid(row=0,column=0,sticky=NSEW)
+    Button(splash_page,relief=FLAT,image=bg_firt,command=pag_login).grid(row=0,column=0,sticky=NSEW)
 
-    fullfram = Frame(root,bg='#ff6b1e')
-    fullfram.rowconfigure((0,1,2,3),weight=1)
-    fullfram.columnconfigure((0,1,2,3),weight=1)
+def buttom_all():
+    print('Hello')
+    global buttom_click_login,buttom_click_pag2,buttom_click_register,buttom_signup
+
+    def buttom_click_login(x,y):
+        global Label_buttom
+
+        Label_buttom = Label(splash_page,relief=FLAT,image=buttom_login,width=327,height=62,bg='#ffffff',border=0)
+        Label_buttom.place(x=x,y=y)
+        def on_enter(e):
+            Label_buttom.config(cursor='hand2')
+            buttom_login.configure(file='image_2\\buttom_act_login.png')
+        def on_leave(e):
+            buttom_login.configure(file='image_2\\buttom_login.png')
+        Label_buttom.bind("<Enter>",on_enter)
+        Label_buttom.bind("<Leave>",on_leave)
+        Label_buttom.bind("<Button-1>", loginclick)
+
+    def buttom_click_register(x,y):
+        global Label_buttom
+
+        Label_buttom = Label(splash_page,relief=FLAT,image=buttom_register,width=327,height=62,bg='#ffffff',border=0)
+        Label_buttom.place(x=x,y=y)
+        def on_enter(e):
+            Label_buttom.config(cursor='hand2')
+            buttom_register.configure(file='image_2\\register_buttom_act.png')
+        def on_leave(e):
+            buttom_register.configure(file='image_2\\register_buttom.png')
+        Label_buttom.bind("<Enter>",on_enter)
+        Label_buttom.bind("<Leave>",on_leave)
+        Label_buttom.bind("<Button-1>", register)
     
-    rigt_frame = Frame(fullfram,bg='#ff6b1e')
-    rigt_frame.rowconfigure((0,1,2,3,4,5,6,7,8,9),weight=1)
-    rigt_frame.columnconfigure((0,1,2,3,4,5),weight=1)
+    def buttom_click_pag2(x,y):
+        global Label_buttom
+        Label_buttom = Label(splash_page,relief=FLAT,image=buttom_image,width=327,height=62,bg='#ffffff',border=0)
+        Label_buttom.place(x=x,y=y)
+        def on_enter(e):
+            Label_buttom.config(cursor='hand2')
+            buttom_image.configure(file='image_2\\buttom_act.png')
+        def on_leave(e):
+            buttom_image.configure(file='image_2\\buttom.png')
+        Label_buttom.bind("<Enter>",on_enter)
+        Label_buttom.bind("<Leave>",on_leave)
+        Label_buttom.bind("<Button-1>", pag3)
+
+    def buttom_signup(x,y):
+        global Label_buttom
+
+        Label_buttom = Label(splash_page,relief=FLAT,image=buttom_register,width=327,height=62,bg='#ffffff',border=0)
+        Label_buttom.place(x=x,y=y)
+        def on_enter(e):
+            Label_buttom.config(cursor='hand2')
+            buttom_register.configure(file='image_2\\register_buttom_act.png')
+        def on_leave(e):
+            buttom_register.configure(file='image_2\\register_buttom.png')
+        Label_buttom.bind("<Enter>",on_enter)
+        Label_buttom.bind("<Leave>",on_leave)
+        Label_buttom.bind("<Button-1>", registration_data)
 
 
-    Label(rigt_frame,image=pic).grid(row=0,column=0,rowspan=10,columnspan=7,sticky=NSEW)
-    Label(rigt_frame,text='Login',font='Helvetica 38 bold',fg='#ffffff',bg='#ff6b1e').grid(row=2,column=5,sticky=NW)
-    Label(rigt_frame,text='Username',font='Helvetica 14 bold',fg='#ffffff',bg='#ff6b1e').grid(row=2,column=5,sticky=SW)
-    userentry = Entry(rigt_frame,bg='#E4FBFF',fg='#000000',width=20,textvariable=userinfo)
-    userentry.grid(row=3,column=5,columnspan=2,sticky=NW)
-    Label(rigt_frame,text='Password',font='Helvetica 14 bold',fg='#ffffff',bg='#ff6b1e').grid(row=3,column=5,sticky=SW)
-    pwdentry = Entry(rigt_frame,bg='#E4FBFF',fg='#000000',width=20,textvariable=pwdinfo)
-    pwdentry.grid(row=4,column=5,columnspan=2,sticky=NW)
+def register(event):
+    Label(splash_page,relief=FLAT,image=bg_signup).place(x=-1,y=-1)
+    global name,gmail,password,password_cf
+    def name_on_enter(e):
+        name.delete(0,'end')
+    def name_on_leave(e):
+        if name.get()=="":
+            name.insert(0,'john wick')
+    
+    def gmail_on_enter(e):
+        gmail.delete(0,'end')
+    def gmail_on_leave(e):
+        if gmail.get()=="":
+            gmail.insert(0,'example@gmail.com')
 
-    Button(rigt_frame,text='Login ',width=17, relief=FLAT,bg='#ffa82b',fg='#000000',borderwidth=5,bd=3).grid(row=5,column=5,columnspan=2,sticky=NW)
+    def pass_on_enter(e):
+        password.delete(0,'end')
+    def pass_on_leave(e):
+        if password.get()=="":
+            password.insert(0,' **********')
+    
+    def pass_cf_on_enter(e):
+        password_cf.delete(0,'end')
+    def pass_cf_on_leave(e):
+        if password_cf.get()=="":
+            password_cf.insert(0,' **********')
+
+    name = Entry(splash_page,width=23,fg='#646464',border=0,bg='#f0f5fa')
+    name.place(x=32,y=279)
+    name.insert(0, 'john wick')
+    name.bind("<FocusIn>",name_on_enter)
+    name.bind("<FocusOut>",name_on_leave)
+    
+    gmail = Entry(splash_page,width=23,fg='#646464',border=0,bg='#f0f5fa')
+    gmail.place(x=32,y=390)
+    gmail.insert(0, 'example@gmail.com')
+    gmail.bind("<FocusIn>",gmail_on_enter)
+    gmail.bind("<FocusOut>",gmail_on_leave)
+
+    password = Entry(splash_page,width=23,fg='#646464',border=0,bg='#f0f5fa')
+    password.place(x=32,y=500)
+    password.insert(0, ' **********')
+    password.bind("<FocusIn>",pass_on_enter)
+    password.bind("<FocusOut>",pass_on_leave)
+
+    password_cf = Entry(splash_page,width=23,fg='#646464',border=0,bg='#f0f5fa')
+    password_cf.place(x=32,y=610)
+    password_cf.insert(0, ' **********')
+    password_cf.bind("<FocusIn>",pass_cf_on_enter)
+    password_cf.bind("<FocusOut>",pass_cf_on_leave)
+    buttom_signup(24,710)
+
+def registration_data() :
+    if name.get() == '':
+        messagebox.showwarning('Admin:','Please enter firstname')
+        name.focus_force()
+    elif gmail.get() == '' :
+        messagebox.showwarning('Admin:','Please enter lastname')
+        gmail.focus_force()
+    elif password.get() == '':
+        messagebox.showwarning('Admin:','Please select year')
+        password.focus_force()
+    elif password_cf.get() == '':
+        messagebox.showwarning('Admin:','Please select year')
+        password_cf.focus_force()
+    elif len(password.get()) == 10 and password.get().isdigit() :
+        if password.get() == password_cf.get():
+            ins_sql = "INSERT INTO students VALUES(?,?,?,?,?,?,?)"
+            param = [name.get(),gmail.get(),password.get()]
+            cursor.execute(ins_sql,param)
+            conn.commit()
+            retrivedata()
+            messagebox.showinfo("Admin","Registration Successfully")
 
 
-    Label(rigt_frame,text="Don't have an account? ",font='Helvetica 14 bold',fg='#ffffff',bg='#ff6b1e',activebackground='#ff6b1e').grid(row=6,column=5,sticky=SW,padx=20)
-    Button(rigt_frame,text='Register Now',bg='#ff6b1e',relief="flat",fg='#ffa82b',font='Helvetica 14 bold underline').grid(row=7,column=5,columnspan=4,sticky=NW,padx=60)
+        
 
-    rigt_frame.grid(row=0,column=0,columnspan=4,rowspan=4,sticky=NSEW)
-    fullfram.grid(row=0,column=0,columnspan=4,rowspan=4,sticky=NSEW)
+    
+
+    
+
+def pag_login():
+    Label(splash_page,relief=FLAT,image=login_pag).place(x=-1,y=-1)
+    global gmail,password
+    def gmail_on_enter(e):
+        gmail.delete(0,'end')
+    def gmail_on_leave(e):
+        if gmail.get()=="":
+            gmail.insert(0,'example@gmail.com')
+    
+    def password_on_enter(e):
+        password.delete(0,'end')
+    def password_on_leave(e):
+        if password.get()=="":
+            password.insert(0,'*******')
+
+    gmail = Entry(splash_page,width=25,fg='#646464',border=0,bg='#f0f5fa')
+    gmail.place(x=30,y=287)
+    gmail.insert(0, 'example@gmail.com')
+    gmail.bind("<FocusIn>",gmail_on_enter)
+    gmail.bind("<FocusOut>",gmail_on_leave)
+    
+    password = Entry(splash_page,width=25,fg='#646464',border=0,bg='#f0f5fa')
+    password.place(x=30,y=400)
+    password.insert(0, '*************')
+    password.bind("<FocusIn>",password_on_enter)
+    password.bind("<FocusOut>",password_on_leave)
+    buttom_click_login(24,530)
+    Label(splash_page,text='----------------- or -----------------',fg='#646464',bg='#ffffff').place(x=30,y=620)
+    buttom_click_register(24,680)
+    
+
+def loginclick(event) :
+    global user_result
+
+    if gmail.get() == "" :
+        messagebox.showwarning("Admin:","Please enter Username")
+        gmail.focus_force()
+    else :
+        sql = "select * from login where user_gmail=?"
+        cursor.execute(sql,[gmail.get()])
+        result = cursor.fetchall()
+        if result :
+            if password.get() == "" :
+                messagebox.showwarning("Admin:","Please enter password")
+                password.focus_force()
+            else :
+                sql = "select * from login where user_gmail=? and password=? "
+                cursor.execute(sql,[gmail.get(),password.get()])   #case1
+                user_result = cursor.fetchone()
+                if user_result :
+                    messagebox.showinfo("Admin:","Login Successfully")
+                    print(user_result)
+                    gmail.delete(0,END)
+                    password.delete(0,END)
+                    page_2()
+                else :
+                    messagebox.showerror("Admin:","Username or Password is invalid.")
+                    gmail.select_range(0,END)
+                    password.focus_force()
+        else :
+            messagebox.showerror("Admin:","Username not found\nPlease register before Login")
+            gmail.select_range(0,END)
+            gmail.focus_force()
+
+def page_2():
+    Label(splash_page,relief=FLAT,image=page2).grid(row=0,column=0,sticky=NSEW)
+    buttom_click_pag2(24,675)
+
+def pag3(event):
+        Label(splash_page,relief=FLAT,image=page3).grid(row=0,column=0,sticky=NSEW)
+        
 
 
+def retrivedata() :
+    sql = "select * from students"
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    print("Total row = ",len(result))
+    for i,data in enumerate(result) :
+        print("Row#",i+1,data)
 
+
+creatconnection()
 root = mainwindow()
 
-pic = PhotoImage(file='image\\Untitled-1.png').subsample(1,1)
-but_1 = PhotoImage(file='image\\buttom.png').subsample(1,1)
+logo = PhotoImage(file='image_2\logo jukjik 02.png').subsample(3,3)
+bg_firt = PhotoImage(file='image_2\Bg_firt.png').subsample(1,1)
+bg_signup = PhotoImage(file='image_2\\bg_signup.png').subsample(1,1)
+page2 = PhotoImage(file='image_2\page_2.png').subsample(1,1)
+page3 = PhotoImage(file='image_2\page_3.png').subsample(1,1)
+buttom_image = PhotoImage(file='image_2\\buttom.png')
+buttom_login = PhotoImage(file='image_2\\buttom_login.png')
+buttom_register = PhotoImage(file='image_2\\register_buttom.png')
+login_pag = PhotoImage(file='image_2\loing.png')
 
+firt_pace()
+buttom_all()
+root.mainloop()
 
-loginlayout()
-root.mainloop()                                                                                                                                                                                                                                
