7cedf67901b43a216bbd8216ac0f20ffdc707a18
Phiphat050647 <112453645+Phiphat050647@users.noreply.github.com>
2023-05-08 17:08:47 +0700

    ล่าสัส

--- Project_Cs311_JubJib.py
+++ Project_Cs311_JubJib.py
@@ -1,11 +1,12 @@
 from tkinter import *
 from tkinter import messagebox
 from tkinter import ttk
+import tkinter.font as tkFont
 
 def mainwindow():
     root = Tk()
     w = 1080
-    h = 1920
+    h = 800
     x = root.winfo_screenwidth()/2 - w/2
     y = root.winfo_screenheight()/2 - h/2
     root.geometry("%dx%d+%d+%d"%(w,h,x,y))
@@ -14,36 +15,48 @@ def mainwindow():
     root.option_add('*font',"Helvetica 18 bold")
     root.rowconfigure((0,1,2,3),weight=1)
     root.columnconfigure((0,1,2,3),weight=1)
+    
     return root
 
 def loginlayout() :
-    global loginframe,userinfo,pwdinfo,userentry,pwdentry
+    global fullfram,userinfo,pwdinfo
     userinfo = StringVar()
-    pwdinfo = StringVar()
+    pwdinfo = StringVar()       
 
-    fullfram = Frame(root,bg='#28527A')
+    fullfram = Frame(root,bg='#ff6b1e')
     fullfram.rowconfigure((0,1,2,3),weight=1)
     fullfram.columnconfigure((0,1,2,3),weight=1)
-    loginframe = Frame(fullfram,bg='#709FB0')
-    loginframe.rowconfigure((0,1,2,3),weight=1)
-    loginframe.columnconfigure((0,1,2),weight=1)
     
-    Label(loginframe,font="Garamond 26 bold",bg='#709FB0').grid(row=0,columnspan=4)
-    Label(loginframe,text='Accoount Login',fg='#ffffff',bg='#709FB0').grid(row=1,columnspan=4)
-    Label(loginframe,text="Username :",bg='#709FB0',fg='#ffffff').grid(row=2,column=1,sticky=E)
-    Label(loginframe,text="Password :",bg='#709FB0',fg='#ffffff').grid(row=3,column=1,sticky=E)
-    userentry = Entry(loginframe,bg='#E4FBFF',fg='#000000',width=20,textvariable=userinfo)
-    pwdentry = Entry(loginframe,bg='#E4FBFF',fg='#000000',width=20,show='*',textvariable=pwdinfo)
-    userentry.grid(row=2,column=2,sticky='w',padx=20)
-    pwdentry.grid(row=3,column=2,sticky='w',padx=20)
-    Button(loginframe,text="Exit",width=7,command=quit).grid(row=4,column=0,columnspan=2,pady=20,ipady=5)
-    Button(loginframe,text='Login',bg="#90EE90",width=7).grid(row=4,column=2,columnspan=2,pady=20,ipady=5,sticky=E,padx=20)
+    rigt_frame = Frame(fullfram,bg='#ff6b1e')
+    rigt_frame.rowconfigure((0,1,2,3,4,5,6,7,8,9),weight=1)
+    rigt_frame.columnconfigure((0,1,2,3,4,5),weight=1)
+
+
+    Label(rigt_frame,image=pic).grid(row=0,column=0,rowspan=10,columnspan=7,sticky=NSEW)
+    Label(rigt_frame,text='Login',font='Helvetica 38 bold',fg='#ffffff',bg='#ff6b1e').grid(row=2,column=5,sticky=NW)
+    Label(rigt_frame,text='Username',font='Helvetica 14 bold',fg='#ffffff',bg='#ff6b1e').grid(row=2,column=5,sticky=SW)
+    userentry = Entry(rigt_frame,bg='#E4FBFF',fg='#000000',width=20,textvariable=userinfo)
+    userentry.grid(row=3,column=5,columnspan=2,sticky=NW)
+    Label(rigt_frame,text='Password',font='Helvetica 14 bold',fg='#ffffff',bg='#ff6b1e').grid(row=3,column=5,sticky=SW)
+    pwdentry = Entry(rigt_frame,bg='#E4FBFF',fg='#000000',width=20,textvariable=pwdinfo)
+    pwdentry.grid(row=4,column=5,columnspan=2,sticky=NW)
+
+    Button(rigt_frame,text='Login ',width=17, relief=FLAT,bg='#ffa82b',fg='#000000',borderwidth=5,bd=3).grid(row=5,column=5,columnspan=2,sticky=NW)
+
+
+    Label(rigt_frame,text="Don't have an account? ",font='Helvetica 14 bold',fg='#ffffff',bg='#ff6b1e',activebackground='#ff6b1e').grid(row=6,column=5,sticky=SW,padx=20)
+    Button(rigt_frame,text='Register Now',bg='#ff6b1e',relief="flat",fg='#ffa82b',font='Helvetica 14 bold underline').grid(row=7,column=5,columnspan=4,sticky=NW,padx=60)
+
+    rigt_frame.grid(row=0,column=0,columnspan=4,rowspan=4,sticky=NSEW)
     fullfram.grid(row=0,column=0,columnspan=4,rowspan=4,sticky=NSEW)
-    loginframe.grid(row=1,column=1,columnspan=2,rowspan=2,sticky='news')
-    userentry.focus_force()
+
 
 
 root = mainwindow()
 
+pic = PhotoImage(file='image\\Untitled-1.png').subsample(1,1)
+but_1 = PhotoImage(file='image\\buttom.png').subsample(1,1)
+
+
 loginlayout()
-root.mainloop()
+root.mainloop()                                                                                                                                                                                                                                
